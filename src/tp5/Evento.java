f4c8b444d093750c07ed67a4f229e2a639a09442
matiasp95 <matiaspaglia@gmail.com>
2021-10-24 21:22:40 -0300

    Cambios iniciales para TP 5

---
+++ src/tp5/Evento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp5;
+
+/**
+ *
+ * @author Matias
+ */
+public enum Evento {
+        Inicio,
+    LlegaPedido,
+    FinA1,
+    FinA2,
+    FinA3,
+    FinA4,
+    FinA5
+    }
--- src/tp5/Fila.java
+++ src/tp5/Fila.java
@@ -5,382 +5,294 @@
  */
 package tp5;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.commons.math3.distribution.TDistribution;
 
 /**
  *
  * @author Usuario
  */
-public class Fila implements IFila {
+public class Fila {
 
-    double randomA1 = 0;
-    double randomA2 = 0;
-    double randomA3 = 0;
-    double randomA4 = 0;
-    double randomA5 = 0;
-    double tiempoA1 = 0;
-    double tiempoA2 = 0;
-    double tiempoA3 = 0;
-    double tiempoA4 = 0;
-    double tiempoA5 = 0;
-    double diferencia = 0;
-    double contadorN = 0;
-    double acumulador = 0;
-    double promedio = 0;
-    double fin = 0;
-    int prob45 = 0;
-    double varianza = 0;
-    double tStudentFormula;
-    boolean C1 = false; //A1 - A4 - A5
-    boolean C2 = false; //A2 - A5
-    boolean C3 = false; //A3 
-    double tiempoMasTardioA1;
-    double tiempoMasTardioA2;
-    double tiempoMasTardioA3;
-    double tiempoMasTardioA4;
-    double tiempoMasTardioA5;
-    double[] contadorCC = {0.0, 0.0, 0.0, 0.0, 0.0};
+    double reloj = 0;
+    Evento e;
+    int material;
+    int proximoMaterial ;
+    double rndPedido;
+    double tiempoEntreLlegadas;
+    double proxLlegada;
+    Actividad A1;
+    Actividad A2;
+    Actividad A3;
+    Actividad A4;
+    Actividad A5;
+    int colaA3;
+    int colaA5;
+    int tareasTerminadas;
+    int contadorN;
+    
 
     double finA4 = 0;
-    IActividad A1;
-    IActividad A2;
-    IActividad A3;
-    IActividad A4;
-    IActividad A5;
+    IActividad llegadaActividadCalc;
 
-    public Fila(IActividad a1, IActividad a2, IActividad a3, IActividad a4, IActividad a5) {
-        this.A1 = a1;
-        this.A2 = a2;
-        this.A3 = a3;
-        this.A4 = a4;
-        this.A5 = a5;
+
+    public Fila(Evento e, int material, int proximoMaterial, double rndPedido, double tiempoEntreLlegadas, double proxLlegada, Actividad A1, Actividad A2, Actividad A3, Actividad A4, Actividad A5, int colaA3, int colaA5, int tareasTerminadas, int contadorN, IActividad llegadaActividadCalc) {
+        this.e = e;
+        this.material = material;
+        this.proximoMaterial = proximoMaterial;
+        this.rndPedido = rndPedido;
+        this.tiempoEntreLlegadas = tiempoEntreLlegadas;
+        this.proxLlegada = proxLlegada;
+        this.A1 = A1;
+        this.A2 = A2;
+        this.A3 = A3;
+        this.A4 = A4;
+        this.A5 = A5;
+        this.colaA3 = colaA3;
+        this.colaA5 = colaA5;
+        this.tareasTerminadas = tareasTerminadas;
+        this.contadorN = contadorN;
+        this.llegadaActividadCalc = llegadaActividadCalc;
     }
 
-    public void CalcularPrimeraFila() {
-        randomA1 = Math.random();
-
-        randomA2 = Math.random();
-
-        randomA3 = Math.random();
-
-        randomA4 = Math.random();
-
-        randomA5 = Math.random();
-
-        tiempoA1 = A1.calcularTiempo(randomA1);
-        tiempoA2 = A2.calcularTiempo(randomA2);
-        tiempoA3 = A3.calcularTiempo(randomA3);
-        tiempoA4 = A4.calcularTiempo(randomA4);
-        finA4 = tiempoA4 + tiempoA1;
-        tiempoA5 = A5.calcularTiempo(randomA5);
-        if (tiempoA2 >= finA4) {
-            fin = tiempoA5 + tiempoA2;
-            C2 = true;
-            C1 = false;
-            C3 = false;
-            if (tiempoA3 > fin) {
-                fin = tiempoA3;
-                C2 = false;
-                C1 = false;
-                C3 = true;
-            }
-
-        } else {
-            fin = tiempoA5 + finA4;
-            C2 = false;
-            C1 = true;
-            C3 = false;
-            if (tiempoA3 > fin) {
-                fin = tiempoA3;
-                C2 = false;
-                C1 = false;
-                C3 = true;
-            }
-        }
-
-        if (C1 == true) {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = 0;
-            tiempoMasTardioA3 = fin - tiempoA3;
-            tiempoMasTardioA2 = tiempoMasTardioA5 - tiempoA2;
-            contadorCC[0] += 1.0;
-            contadorCC[3] += 1.0;
-            contadorCC[4] += 1.0;
-        } else if (C2 == true) {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = tiempoMasTardioA4 - tiempoA1;
-            tiempoMasTardioA3 = fin - tiempoA3;
-            tiempoMasTardioA2 = 0;
-            contadorCC[1] += 1.0;
-            contadorCC[4] += 1.0;
-        } else {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = tiempoA4 - tiempoA1;
-            tiempoMasTardioA3 = 0;
-            tiempoMasTardioA2 = tiempoMasTardioA5 - tiempoA2;
-            if (tiempoMasTardioA1 < 0) {
-                tiempoMasTardioA1 = 0;
-            }
-            contadorCC[2] += 1.0;
-        }
-        contadorN = 1;
-        acumulador = fin;
-        promedio = fin;
-        //varianza = (1/(contadorN-1))*(contadorN/(contadorN-1)*(promedio-fin));
-        //System.out.println(cantidadPasajerosPresentes + " " + promedio + " " + acumulador);
-
+    public Fila() {
+    
     }
 
-    public void CalcularNuevaFila(double contadorN, double promedio, double varianza) {
-        prob45 = 0;
 
-        randomA1 = Math.random();
-
-        randomA2 = Math.random();
-
-        randomA3 = Math.random();
-
-        randomA4 = Math.random();
-
-        randomA5 = Math.random();
-
-        tiempoA1 = A1.calcularTiempo(randomA1);
-        tiempoA2 = A2.calcularTiempo(randomA2);
-        tiempoA3 = A3.calcularTiempo(randomA3);
-        tiempoA4 = A4.calcularTiempo(randomA4);
-        finA4 = tiempoA4 + tiempoA1;
-        tiempoA5 = A5.calcularTiempo(randomA5);
-        if (tiempoA2 >= finA4) {
-            fin = tiempoA5 + tiempoA2;
-            C2 = true;
-            C1 = false;
-            C3 = false;
-            if (tiempoA3 > fin) {
-                fin = tiempoA3;
-                C2 = false;
-                C1 = false;
-                C3 = true;
-            }
-
-        } else {
-            fin = tiempoA5 + finA4;
-            C2 = false;
-            C1 = true;
-            C3 = false;
-            if (tiempoA3 > fin) {
-                fin = tiempoA3;
-                C2 = false;
-                C1 = false;
-                C3 = true;
-            }
-        }
-
-        if (C1 == true) {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = 0;
-            tiempoMasTardioA3 = fin - tiempoA3;
-            tiempoMasTardioA2 = tiempoMasTardioA5 - tiempoA2;
-            contadorCC[0] += 1.0;
-            contadorCC[3] += 1.0;
-            contadorCC[4] += 1.0;
-        } else if (C2 == true) {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = tiempoMasTardioA4 - tiempoA1;
-            tiempoMasTardioA3 = fin - tiempoA3;
-            tiempoMasTardioA2 = 0;
-            contadorCC[1] += 1.0;
-            contadorCC[4] += 1.0;
-        } else {
-            tiempoMasTardioA5 = fin - tiempoA5;
-            tiempoMasTardioA4 = tiempoMasTardioA5 - tiempoA4;
-            tiempoMasTardioA1 = tiempoA4 - tiempoA1;
-            tiempoMasTardioA3 = 0;
-            tiempoMasTardioA2 = tiempoMasTardioA5 - tiempoA2;
-            if (tiempoMasTardioA1 < 0) {
-                tiempoMasTardioA1 = 0;
-            }
-            contadorCC[2] += 1.0;
-        }
-        this.contadorN = contadorN + 1;
-        this.acumulador = acumulador + fin;
-        this.promedio = 1 / contadorN * ((contadorN - 1) * promedio + fin);
-        this.varianza = (1 / (this.contadorN - 1)) * ((this.contadorN - 2) * varianza + (this.contadorN / (this.contadorN - 1) * Math.pow((promedio - fin), 2)));
-        TDistribution tds = new TDistribution(this.contadorN - 1);
-        this.tStudentFormula = this.promedio + tds.inverseCumulativeProbability(0.975) * Math.sqrt(this.varianza) / Math.sqrt(this.contadorN);
-
+    public void CalcularPrimeraFila(double lambda) {
+       e = e.Inicio;
+       material = 0;
+       proximoMaterial = 1;
+       llegadaActividadCalc = new ActividadExponencial(1/lambda);
+       rndPedido = Math.random();
+       tiempoEntreLlegadas = llegadaActividadCalc.calcularTiempo(rndPedido);
+       proxLlegada = reloj + tiempoEntreLlegadas;
+       
+       IActividad paraA1 = new ActividadUniforme(20,30);
+       IActividad paraA2 = new ActividadUniforme(30,50);
+       IActividad paraA3 = new ActividadExponencial(30);
+       IActividad paraA4 = new ActividadUniforme(10,20);
+       IActividad paraA5 = new ActividadExponencial(5);
+       
+       A1 = new Actividad(Estado.Libre,0,0, paraA1);
+       A2 = new Actividad(Estado.Libre,0,0, paraA2);
+       A3 = new Actividad(Estado.Libre,0,0, paraA3);
+       A4 = new Actividad(Estado.Libre,0,0, paraA4);
+       A5 = new Actividad(Estado.Libre,0,0, paraA5);
+       
+       colaA3 = 0;
+       colaA5 = 0;
+       tareasTerminadas = 0;
+       contadorN = 1;
     }
 
-    public long getSemilla() {
-        return -1;
+    public void CalcularNuevaFila() {
+        this.reloj = menorTiempo();
+        
+       if(reloj == proxLlegada){
+           e = e.LlegaPedido;
+       }else if(reloj == A1.getProxFin()){
+           e = e.FinA1;
+       }else if (reloj == A2. getProxFin()){
+           e = e.FinA2;
+       }else if (reloj == A3. getProxFin()){
+           e = e.FinA3;
+       }else if (reloj == A4. getProxFin()){
+           e = e.FinA4;
+       }else 
+           e = e.FinA5;
+       
+       if (e == Evento.LlegaPedido){
+           material+=1;
+           proximoMaterial+=1;
+           rndPedido = Math.random();
+           tiempoEntreLlegadas = llegadaActividadCalc.calcularTiempo(rndPedido);
+           proxLlegada += tiempoEntreLlegadas;
+           
+           if(A1.getE()== Estado.Libre){
+               A1.setMaterial(1);
+               double randA1 = Math.random();
+               A1.setRnd(randA1);
+               double tiempoCalculadoA1 = A1.getCalcActividad().calcularTiempo(randA1);
+           A1.setTiempoAtencion(tiempoCalculadoA1);
+           A1.setProxFin(reloj+tiempoCalculadoA1);
+           A1.setColaUno(0);
+           }
+           A1.setE(Estado.Ocupado);
+           
+            if(A2.getE()== Estado.Libre){
+               A2.setMaterial(1);
+               double randA1 = Math.random();
+               A2.setRnd(randA1);
+               double tiempoCalculadoA1 = A2.getCalcActividad().calcularTiempo(randA1);
+           A2.setTiempoAtencion(tiempoCalculadoA1);
+           A2.setProxFin(reloj+tiempoCalculadoA1);
+           A2.setColaUno(0);
+           }
+           A2.setE(Estado.Ocupado);
+           
+            if(A3.getE()== Estado.Libre){
+               A3.setMaterial(1);
+               double randA1 = Math.random();
+               A3.setRnd(randA1);
+               double tiempoCalculadoA1 = A3.getCalcActividad().calcularTiempo(randA1);
+           A3.setTiempoAtencion(tiempoCalculadoA1);
+           A3.setProxFin(reloj+tiempoCalculadoA1);
+           A3.setColaUno(0);
+           }
+           A3.setE(Estado.Ocupado);
+           
+       }
+       if(e == Evento.FinA1){
+           double randA1 = Math.random();
+           A1.setRnd(randA1);
+           double tiempoCalculadoA1 = A1.getCalcActividad().calcularTiempo(randA1);
+           A1.setTiempoAtencion(tiempoCalculadoA1);
+           A1.setProxFin(reloj+tiempoCalculadoA1);
+       }
+       
+       contadorN++;
+
+    }
+    
+    private double menorTiempo(){
+        double min = Math.min(tiempoEntreLlegadas,A1.getProxFin());
+        min = Math.min(min,A2.getProxFin());
+        min = Math.min(min,A3.getProxFin());
+        min = Math.min(min,A4.getProxFin());
+        min = Math.min(min,A5.getProxFin());
+        
+        return min;
     }
 
-    public void setSemilla(long s) {
-
-    }
-
-    public double getRandomA1() {
-        return randomA1;
-    }
-
-    public void setRandomA1(double randomA1) {
-        this.randomA1 = randomA1;
-    }
-
-    public double getRandomA2() {
-        return randomA2;
-    }
-
-    public void setRandomA2(double randomA2) {
-        this.randomA2 = randomA2;
-    }
-
-    public double getRandomA3() {
-        return randomA3;
-    }
-
-    public void setRandomA3(double randomA3) {
-        this.randomA3 = randomA3;
-    }
-
-    public double getRandomA4() {
-        return randomA4;
-    }
-
-    public void setRandomA4(double randomA4) {
-        this.randomA4 = randomA4;
-    }
-
-    public double getRandomA5() {
-        return randomA5;
-    }
-
-    public void setRandomA5(double randomA5) {
-        this.randomA5 = randomA5;
-    }
-
-    public double getTiempoA1() {
-        return tiempoA1;
-    }
-
-    public void setTiempoA1(double tiempoA1) {
-        this.tiempoA1 = tiempoA1;
-    }
-
-    public double getTiempoA2() {
-        return tiempoA2;
-    }
-
-    public void setTiempoA2(double tiempoA2) {
-        this.tiempoA2 = tiempoA2;
-    }
-
-    public double getTiempoA3() {
-        return tiempoA3;
-    }
-
-    public void setTiempoA3(double tiempoA3) {
-        this.tiempoA3 = tiempoA3;
-    }
-
-    public double getTiempoA4() {
-        return tiempoA4;
-    }
-
-    public void setTiempoA4(double tiempoA4) {
-        this.tiempoA4 = tiempoA4;
-    }
-
-    public double getTiempoA5() {
-        return tiempoA5;
-    }
-
-    public void setTiempoA5(double tiempoA5) {
-        this.tiempoA5 = tiempoA5;
-    }
-
-    public double getDiferencia() {
-        return diferencia;
-    }
-
-    public void setDiferencia(double diferencia) {
-        this.diferencia = diferencia;
-    }
-
-    public double getContadorN() {
+    public int getContadorN() {
         return contadorN;
     }
 
-    public void setContadorN(double contadorN) {
+    public void setContadorN(int contadorN) {
         this.contadorN = contadorN;
     }
 
-    public double getAcumulador() {
-        return acumulador;
+    public double getReloj() {
+        return reloj;
     }
 
-    public void setAcumulador(double acumulador) {
-        this.acumulador = acumulador;
+    public void setReloj(double reloj) {
+        this.reloj = reloj;
     }
 
-    public double getPromedio() {
-        return promedio;
+    public Evento getE() {
+        return e;
     }
 
-    public void setPromedio(double promedio) {
-        this.promedio = promedio;
+    public void setE(Evento e) {
+        this.e = e;
     }
 
-    public IActividad getA1() {
+    public int getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(int material) {
+        this.material = material;
+    }
+
+    public int getProximoMaterial() {
+        return proximoMaterial;
+    }
+
+    public void setProximoMaterial(int proximoMaterial) {
+        this.proximoMaterial = proximoMaterial;
+    }
+
+    public double getRndPedido() {
+        return rndPedido;
+    }
+
+    public void setRndPedido(double rndPedido) {
+        this.rndPedido = rndPedido;
+    }
+
+    public double getTiempoEntreLlegadas() {
+        return tiempoEntreLlegadas;
+    }
+
+    public void setTiempoEntreLlegadas(double tiempoEntreLlegadas) {
+        this.tiempoEntreLlegadas = tiempoEntreLlegadas;
+    }
+
+    public double getProxLlegada() {
+        return proxLlegada;
+    }
+
+    public void setProxLlegada(double proxLlegada) {
+        this.proxLlegada = proxLlegada;
+    }
+
+    public Actividad getA1() {
         return A1;
     }
 
-    public void setA1(IActividad A1) {
+    public void setA1(Actividad A1) {
         this.A1 = A1;
     }
 
-    public IActividad getA2() {
+    public Actividad getA2() {
         return A2;
     }
 
-    public void setA2(IActividad A2) {
+    public void setA2(Actividad A2) {
         this.A2 = A2;
     }
 
-    public IActividad getA3() {
+    public Actividad getA3() {
         return A3;
     }
 
-    public void setA3(IActividad A3) {
+    public void setA3(Actividad A3) {
         this.A3 = A3;
     }
 
-    public IActividad getA4() {
+    public Actividad getA4() {
         return A4;
     }
 
-    public void setA4(IActividad A4) {
+    public void setA4(Actividad A4) {
         this.A4 = A4;
     }
 
-    public IActividad getA5() {
+    public Actividad getA5() {
         return A5;
     }
 
-    public void setA5(IActividad A5) {
+    public void setA5(Actividad A5) {
         this.A5 = A5;
     }
 
-    public double getFin() {
-        return fin;
+    public int getColaA3() {
+        return colaA3;
     }
 
-    public void setFin(double fin) {
-        this.fin = fin;
+    public void setColaA3(int colaA3) {
+        this.colaA3 = colaA3;
+    }
+
+    public int getColaA5() {
+        return colaA5;
+    }
+
+    public void setColaA5(int colaA5) {
+        this.colaA5 = colaA5;
+    }
+
+    public int getTareasTerminadas() {
+        return tareasTerminadas;
+    }
+
+    public void setTareasTerminadas(int tareasTerminadas) {
+        this.tareasTerminadas = tareasTerminadas;
     }
 
     public double getFinA4() {
@@ -391,56 +303,15 @@ public class Fila implements IFila {
         this.finA4 = finA4;
     }
 
-    public int getProb45() {
-        return prob45;
+    public IActividad getLlegadaActividadCalc() {
+        return llegadaActividadCalc;
     }
 
-    public void setProb45(int prob45) {
-        this.prob45 = prob45;
+    public void setLlegadaActividadCalc(IActividad llegadaActividadCalc) {
+        this.llegadaActividadCalc = llegadaActividadCalc;
     }
 
-    public double getVarianza() {
-        return varianza;
-    }
-
-    public void setVarianza(double varianza) {
-        this.varianza = varianza;
-    }
-
-    public double gettStudentFormula() {
-        return tStudentFormula;
-    }
-
-    public void settStudentFormula(double tStudentFormula) {
-        this.tStudentFormula = tStudentFormula;
-    }
-
-    public double getTiempoMasTardioA1() {
-        return tiempoMasTardioA1;
-    }
-
-    public double getTiempoMasTardioA2() {
-        return tiempoMasTardioA2;
-    }
-
-    public double getTiempoMasTardioA3() {
-        return tiempoMasTardioA3;
-    }
-
-    public double getTiempoMasTardioA4() {
-        return tiempoMasTardioA4;
-    }
-
-    public double getTiempoMasTardioA5() {
-        return tiempoMasTardioA5;
-    }
-
-    public double[] getContadorCC() {
-        return contadorCC;
-    }
-
-    public void setContadorCC(double[] contadorCC) {
-        this.contadorCC = contadorCC;
-    }
+    
+   
 
 }
